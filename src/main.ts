b3155c5f67108131dbae6bfadec876ccd77ce9ff
Alex Reardon <areardon@atlassian.com>
2024-06-11 10:39:24 +1000

    adding drag previews

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import {
   dropTargetForElements,
 } from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
 import { combine } from '@atlaskit/pragmatic-drag-and-drop/combine';
+import { setCustomNativeDragPreview } from '@atlaskit/pragmatic-drag-and-drop/element/set-custom-native-drag-preview';
+import { pointerOutsideOfPreview } from '@atlaskit/pragmatic-drag-and-drop/element/pointer-outside-of-preview';
 
 const items = document.querySelectorAll('[data-task-id]');
 
@@ -12,6 +14,24 @@ const cleanups = Array.from(items)
     const cleanup = combine(
       draggable({
         element,
+        onGenerateDragPreview({ nativeSetDragImage }) {
+          setCustomNativeDragPreview({
+            nativeSetDragImage,
+            getOffset: pointerOutsideOfPreview({
+              x: '16px',
+              y: '8px',
+            }),
+            render({ container }) {
+              // Dynamically creating a more reduce drag preview
+              const preview = document.createElement('div');
+              preview.classList.add('border-solid', 'rounded', 'p-2', 'bg-white');
+              preview.textContent =
+                element.querySelector('[data-task-content]')?.textContent ?? element.textContent;
+
+              container.appendChild(preview);
+            },
+          });
+        },
         onDragStart() {
           element.classList.add('opacity-40');
         },
