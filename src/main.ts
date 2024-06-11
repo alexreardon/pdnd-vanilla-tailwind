049798b7033e8a634641463215da511378b2e0ed
Alex Reardon <areardon@atlassian.com>
2024-06-11 11:47:04 +1000

    handling cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,129 +1,133 @@
 import {
-  draggable,
-  dropTargetForElements,
-  monitorForElements,
-} from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
-import { combine } from '@atlaskit/pragmatic-drag-and-drop/combine';
-import { setCustomNativeDragPreview } from '@atlaskit/pragmatic-drag-and-drop/element/set-custom-native-drag-preview';
-import { pointerOutsideOfPreview } from '@atlaskit/pragmatic-drag-and-drop/element/pointer-outside-of-preview';
-import { getReorderDestinationIndex } from '@atlaskit/pragmatic-drag-and-drop-hitbox/util/get-reorder-destination-index';
-import {
   attachClosestEdge,
-  type Edge,
   extractClosestEdge,
 } from '@atlaskit/pragmatic-drag-and-drop-hitbox/closest-edge';
-import { getDropIndicator } from './drag-preview';
+import { combine } from '@atlaskit/pragmatic-drag-and-drop/combine';
+import {
+  draggable,
+  dropTargetForElements,
+} from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
+import { pointerOutsideOfPreview } from '@atlaskit/pragmatic-drag-and-drop/element/pointer-outside-of-preview';
+import { setCustomNativeDragPreview } from '@atlaskit/pragmatic-drag-and-drop/element/set-custom-native-drag-preview';
 import invariant from 'tiny-invariant';
+import { getDropIndicator } from './drag-preview';
+import type { CleanupFn } from '@atlaskit/pragmatic-drag-and-drop/types';
 
-const items = document.querySelectorAll('[data-task-id]');
+function attachAll(): CleanupFn {
+  const cleanups = Array.from(document.querySelectorAll('[data-task-id]'))
+    .filter((element): element is HTMLElement => element instanceof HTMLElement)
+    .map((element) => {
+      const cleanup = combine(
+        draggable({
+          element,
+          onGenerateDragPreview({ nativeSetDragImage }) {
+            setCustomNativeDragPreview({
+              nativeSetDragImage,
+              getOffset: pointerOutsideOfPreview({
+                x: '16px',
+                y: '8px',
+              }),
+              render({ container }) {
+                // Dynamically creating a more reduce drag preview
+                const preview = document.createElement('div');
+                preview.classList.add('border-solid', 'rounded', 'p-2', 'bg-white');
 
-const cleanups = Array.from(items)
-  .filter((element): element is HTMLElement => element instanceof HTMLElement)
-  .map((element) => {
-    const cleanup = combine(
-      draggable({
-        element,
-        onGenerateDragPreview({ nativeSetDragImage }) {
-          setCustomNativeDragPreview({
-            nativeSetDragImage,
-            getOffset: pointerOutsideOfPreview({
-              x: '16px',
-              y: '8px',
-            }),
-            render({ container }) {
-              // Dynamically creating a more reduce drag preview
-              const preview = document.createElement('div');
-              preview.classList.add('border-solid', 'rounded', 'p-2', 'bg-white');
+                // Use a part of the element as the content for the drag preview
+                preview.textContent =
+                  element.querySelector('[data-task-content]')?.textContent ??
+                  // worst case fallback if we set up our data-* up wrong
+                  element.textContent;
 
-              // Use a part of the element as the content for the drag preview
-              preview.textContent =
-                element.querySelector('[data-task-content]')?.textContent ??
-                // worst case fallback if we set up our data-* up wrong
-                element.textContent;
+                container.appendChild(preview);
+              },
+            });
+          },
+          onDragStart() {
+            element.classList.add('opacity-40');
+          },
+          onDrop() {
+            element.classList.remove('opacity-40');
+          },
+        }),
+        dropTargetForElements({
+          element,
+          canDrop({ source }) {
+            // cannot drop on self
+            if (source.element === element) {
+              return false;
+            }
+            // only accepting tasks
+            return source.element.hasAttribute('data-task-id');
+          },
+          getData({ input }) {
+            return attachClosestEdge(
+              {},
+              {
+                element,
+                input,
+                allowedEdges: ['top', 'bottom'],
+              },
+            );
+          },
+          getIsSticky() {
+            return true;
+          },
+          onDragEnter({ self }) {
+            const closestEdge = extractClosestEdge(self.data);
+            if (!closestEdge) {
+              return;
+            }
+            const indicator = getDropIndicator({ edge: closestEdge, gap: '8px' });
+            element.insertAdjacentElement('afterend', indicator);
+          },
+          onDrag({ self }) {
+            const closestEdge = extractClosestEdge(self.data);
+            if (!closestEdge) {
+              element.nextElementSibling?.remove();
+              return;
+            }
 
-              container.appendChild(preview);
-            },
-          });
-        },
-        onDragStart() {
-          element.classList.add('opacity-40');
-        },
-        onDrop() {
-          element.classList.remove('opacity-40');
-        },
-      }),
-      dropTargetForElements({
-        element,
-        canDrop({ source }) {
-          // cannot drop on self
-          if (source.element === element) {
-            return false;
-          }
-          // only accepting tasks
-          return source.element.hasAttribute('data-task-id');
-        },
-        getData({ input }) {
-          return attachClosestEdge(
-            {},
-            {
-              element,
-              input,
-              allowedEdges: ['top', 'bottom'],
-            },
-          );
-        },
-        getIsSticky() {
-          return true;
-        },
-        onDragEnter({ self }) {
-          const closestEdge = extractClosestEdge(self.data);
-          if (!closestEdge) {
-            return;
-          }
-          const indicator = getDropIndicator({ edge: closestEdge, gap: '8px' });
-          element.insertAdjacentElement('afterend', indicator);
-        },
-        onDrag({ self }) {
-          const closestEdge = extractClosestEdge(self.data);
-          if (!closestEdge) {
+            // don't need to do anything, already have a drop indicator in the right spot
+            if (element.nextElementSibling?.getAttribute('data-edge') === closestEdge) {
+              return;
+            }
+
+            // get rid of the old drop indicator
             element.nextElementSibling?.remove();
-            return;
-          }
 
-          // don't need to do anything, already have a drop indicator in the right spot
-          if (element.nextElementSibling?.getAttribute('data-edge') === closestEdge) {
-            return;
-          }
+            // make a new one
+            const indicator = getDropIndicator({ edge: closestEdge, gap: '8px' });
+            element.insertAdjacentElement('afterend', indicator);
+          },
+          onDragLeave() {
+            element.nextElementSibling?.remove();
+          },
+          onDrop({ self, source }) {
+            element.nextElementSibling?.remove();
 
-          // get rid of the old drop indicator
-          element.nextElementSibling?.remove();
+            const closestEdgeOfTarget = extractClosestEdge(self.data);
 
-          // make a new one
-          const indicator = getDropIndicator({ edge: closestEdge, gap: '8px' });
-          element.insertAdjacentElement('afterend', indicator);
-        },
-        onDragLeave() {
-          element.nextElementSibling?.remove();
-        },
-        onDrop({ self, source }) {
-          element.nextElementSibling?.remove();
+            if (!closestEdgeOfTarget) {
+              return;
+            }
 
-          const closestEdgeOfTarget = extractClosestEdge(self.data);
+            // the "position:relative" container around the item
+            const toMove = source.element.parentElement;
+            invariant(toMove);
 
-          if (!closestEdgeOfTarget) {
-            return;
-          }
+            element.parentElement?.insertAdjacentElement(
+              closestEdgeOfTarget === 'top' ? 'beforebegin' : 'afterend',
+              toMove,
+            );
+          },
+        }),
+      );
+      return cleanup;
+    });
 
-          // the "position:relative" container around the item
-          const toMove = source.element.parentElement;
-          invariant(toMove);
+  return function cleanupAll() {
+    cleanups.forEach((fn) => fn());
+  };
+}
 
-          element.parentElement?.insertAdjacentElement(
-            closestEdgeOfTarget === 'top' ? 'beforebegin' : 'afterend',
-            toMove,
-          );
-        },
-      }),
-    );
-    return cleanup;
-  });
+attachAll();
